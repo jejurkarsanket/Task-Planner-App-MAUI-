cae828ceec495ec5553b0f228aeee293cb8e0650
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-03 02:03:34 +0530

    MAUI Client Changes.

---
+++ ToDoMauiClient/ToDoMauiClient/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToDoMauiClient.DataServices;
+using ToDoMauiClient.Pages;
+
+namespace ToDoMauiClient;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddHttpClient<IRestDataService, RestDataService>();
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddTransient<ManageToDoPage>();
+		return builder.Build();
+	}
+}
