ed06b306d293838dadf51ecba02764e822e205ea
Sanket Jejurkar <jejurkarsanket8@gmail.com>
2022-12-30 23:32:23 +0530

    Added Comment

--- ToDoAPI/ToDoAPI/Data/AppDbContext.cs
+++ ToDoAPI/ToDoAPI/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using ToDoAPI.Models;
 
 namespace ToDoAPI.Data
 {
+    //DB Context
     public class AppDbContext: DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) 
